10dac68570d1abbf8694cfba7aad964dbf660a4e
kanwatanabe <kkk.k10@icloud.com>
2020-02-06 22:14:54 +0900

    Make a basic User model

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
-Rails.application.routes.draw do
+# frozen_string_literal: true
 
+Rails.application.routes.draw do
   get '/signup', to: 'users#new'
 
   root 'static_pages#home'
@@ -8,7 +9,5 @@ Rails.application.routes.draw do
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
 
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
 end
